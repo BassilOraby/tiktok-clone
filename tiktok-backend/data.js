79e35da74282409c92508c1213c167c2342afc11
BassilOraby <beats220@gmail.com>
2021-08-29 12:11:46 -0700

    server.js modification

---
+++ tiktok-backend/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url : "/Videos/v1.mp4",
+        channel : "BassilOraby",
+        song : "Toulouse-Nicky Romero",
+        likes : "953",
+        messages : "568",
+        desc : "Man Under Multicoloured lights",
+        shares : "500"
+    },
+    {
+        url : "/Videos/v2.mp4",
+        channel : "RafehQazi",
+        song : "Ivar Travels The Silk Road-Trevor Morris",
+        likes : "1000000",
+        messages : "50000",
+        desc : "Fashion model with a cold and pale appearance",
+        shares : "7268"
+    }
+]
+
+export default data;
--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose'
+/* in the following import line, we must write data.js not data only,
+this is the way it works with importing js files in nodeJS */
+import data from './data.js';
 
+//App config
 const app = express();
 const port = 9000;
 
+//Middleware
+
+//DB config
+
+//API endpoints
 app.get('/',(req,res) => res.status(200).send('hello world !'))
 
+app.get('/v1/posts', (req,res) => res.status(200).send(data));
+
+
+//Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`))
